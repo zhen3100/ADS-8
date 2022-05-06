dda70f0f6492a956ecae542e90061dbab8a20123
Evgeny Volkov <88347990+zhen3100@users.noreply.github.com>
2022-05-07 00:55:17 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,42 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+void Train::addCage(bool light) {
+        if (first != nullptr && last != nullptr) {
+            last->next = create(light);
+            last->next->prev = last;
+            last = last->next;
+            last->next = first;
+            first->prev = last;
+        } else {
+            first = last = create(light);
+        }
+    }; 
+    int Train::getLength() {
+        first->light = true;
+        while (first->next != nullptr) {
+            if (first->next->light != true) {
+                countOp++;
+                k++;
+                first->next = first->next->next;
+            }
+            else {
+                if (first->next->light) {
+                    first->next->light = false;
+                    countOp++;
+                }
+                for (int i = k; i > 0; i--) {
+                    first->next = first->next->prev;
+                    countOp++;
+                }
+                if (first->next->light!=true) {
+                    break;
+                }
+                first->next = first->next->next;
+                k = 1;
+            }
+        }
+        return k;
+    }
+    int Train::getOpCount() {
+        return countOp;
+    }
